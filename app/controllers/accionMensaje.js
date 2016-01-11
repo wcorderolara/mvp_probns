eac4e3d358fd5807b0a42f82429fa4ab27f000ed
unknown <30202775@gua-hcp-lt258.acshcp.com>
2016-01-11 16:33:55 -0600

    Add AccionMensaje controller

---
+++ app/controllers/accionMensaje.js
@@ -0,0 +1,135 @@
+var models = require('../../models');
+
+exports.getTiposUsuario = function(req, res, next){
+	models.tipoUsuario.findAll({
+		where:{
+			status: 1
+		}
+	}).then(function (response){
+		if(!response){
+			res.status(500);
+			res.json({
+				type:false,
+				data: "Error al obtener los Tipos de Usuario: " + response
+			});
+		}else{
+			res.status(200);
+			res.json({
+				type: true,
+				data: response
+			});
+		};
+	});
+};
+
+exports.getTipoUsuarioById = function(req, res, next){
+	models.tipoUsuario.findOne({
+		where:{
+			id: req.params.id
+		}
+	}).then(function (response){
+		if(!response){
+			res.status(500);
+			res.json({
+				type: false,
+				data: "Error al obtener el Tipo de Usuario"
+			});
+		}else{
+			res.status(200);
+			res.json({
+				type: true,
+				data: response
+			});
+		};
+	});
+};
+
+exports.postTipoUsuario = function(req, res, next){
+	models.tipoUsuario.create({
+		descripcion: req.body.descripcion,
+		status: 1
+	}).then(function (response){
+		if(!response){
+			res.status(500);
+			res.json({
+				type: false,
+				data: "Ha ocurrido un Error: " + response
+			});
+		}else{
+			res.status(200);
+			res.json({
+				type: true,
+				data: "Tipo de Usuario creado Exitosamente"
+			});
+		};
+	});
+};
+
+exports.putTipoUsuario = function(req, res, next){
+	models.tipoUsuario.findOne({
+		where:{
+			id: req.params.id
+		}
+	}).then(function (tipoUsuario){
+		if(!tipoUsuario){
+			res.status(500);
+			res.json({
+				type: false,
+				data: "Tipo de Usuario no encontrado"
+			});
+		}else{
+			tipoUsuario.update({
+				descripcion: req.body.descripcion,
+				status: req.body.status
+			}).then(function (_tipoUsuario){
+				if(!_tipoUsuario){
+					res.status(500);
+					res.json({
+						type: false,
+						data: "Error al actualizar el Tipo de Usuario: " + tipoUsuario.descripcion
+					});
+				}else{
+					res.status(200);
+					res.json({
+						type: true,
+						data: "Tipo de Usuario Actualizado Exitosamente..."
+					});
+				};
+			});
+		};
+	});
+};
+
+exports.deleteTipoUsuario = function(req, res, next){
+	models.tipoUsuario.findOne({
+		where:{
+			id: req.params.id
+		}
+	}).then(function (tipoUsuario){
+		if(!tipoUsuario){
+			res.status(500);
+			res.json({
+				type: false,
+				data: "Error al encontrar el Tipo de Usuario"
+			});
+		}else{
+			tipoUsuario.update({
+				status: 0
+			}).then(function (_tipoUsuario){
+				if(!_tipoUsuario){
+					res.status(500);
+					res.json({
+						type: false,
+						data: "Error al Eliminar el Tipo de Usuario: " + tipoUsuario.descripcion
+					});
+				}else{
+					res.status(200);
+					res.json({
+						type: true,
+						data: "El tipo de Usuario: " + tipoUsuario.descripcion + " ha sido eliminado exitosamente."
+					});
+				};
+			});
+		};
+	});
+};
