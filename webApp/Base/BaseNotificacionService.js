4f8fcb9459f5b8e96b4dba801e7de1f9ab2b642d
Walter Cordero <wdlara.8789@gmail.com>
2016-03-07 22:38:20 -0600

    new webApp structure

--- webApp/Base/BaseNotificacionService.js
+++
@@ -1,7 +0,0 @@
-var BaseNotificationService = Class.extend({
-  success: function(message, position){},
-  error: function(message, position){},
-  info: function(message, position){},
-  warging: function(message, position){},
-  loading: function(){}
-})
--- webApp/Base/BaseService.js
+++
@@ -1,7 +0,0 @@
-var BaseService = Class.extend({
-  get: function(params){},
-  getList: function(params){},
-  post: function(params){},
-  put: function(params){},
-  postFile: function(params){}
-})
